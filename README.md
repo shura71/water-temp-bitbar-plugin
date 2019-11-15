4e9d75ac339febc0bf95f5c00e958744753cfe99
Shura Scherban <shura.71@gmail.com>
2019-11-15 12:21:09 +0200

    Imported sources

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# water-temp-bitbar-plugin
-Plugin for BitBar. Water temperature in Ukrainian rivers from meteo.gov.ua
+## Water temperature
+Plugin for [BitBar](https://github.com/matryer/bitbar). 
+Water temperature in Ukrainian rivers from [meteo.gov.ua](https://meteo.gov.ua).
+
+![BitBar Example showing Water temperature plugin](https://github.com/shura71/water-temp-bitbar-plugin/master/water-temp.png)
+
+### Possible station ids
+- 80986 - Dnipro
+- 80136 - Desna
+- 80295 - Ros'
+- 81017 - Dniester
+- 78288 - Seversky Donets
+- 81348 - Southern Bug
+- 42801 - Danube
+
+### How to use
+ - Install ruby
+ - Install gem [nokogiri](http://www.nokogiri.org/tutorials/installing_nokogiri.html) and [openssl](https://github.com/ruby/openssl)
+ - Drop water.30m.rb file into your BitBar plugins folder
+ - Make sure it's executable (in Terminal, do chmod +x water.30m.rb)
+ - Edit water.30m.rb and change station id for what you needed
---
+++ water.4h.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby -W0
+# <bitbar.title>Water temperature in Ukrainian rivers</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Shura Scherban</bitbar.author>
+# <bitbar.author.github>shura71</bitbar.author.github>
+# <bitbar.desc>Water temperature in Ukrainian rivers from meteo.gov.ua</bitbar.desc>
+# <bitbar.image>https://github.com/shura71/water-temp-bitbar-plugin/master/water-temp.png</bitbar.image>
+# <bitbar.dependencies>ruby</bitbar.dependencies>
+# <bitbar.abouturl>https://github.com/shura71/water-temp-bitbar-plugin</bitbar.abouturl>
+# encoding: utf-8
+require 'nokogiri'
+require 'openssl'
+require 'open-uri'
+
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+STATION_ID = '80986'
+
+doc = Nokogiri::XML(open('https://meteo.gov.ua/kml/kml_hydro_warn.kml'))
+begin
+  kyiv = doc.css('Placemark').reject {|p| p.css('name').text != STATION_ID }.map {|p| p.css('description').text }
+  text = kyiv[0].gsub(/(<b>|<\/b>)/,'').gsub(/<br>/,"\n").gsub(/\t/,'').gsub(/^\s+/,'').gsub(/<!--([^>]+)-->/,'').gsub(/\n\n\n/,'')
+  lines = ['---']
+  text.split(/\n/).each do |line|
+    next if /(Область)/u =~ line
+    lines.unshift("〰️ #{$1}℃") and next if /Температура води (\d+)/u =~ line
+    lines << line
+  end
+  lines << '---'
+  lines << 'Укргiдрометцентр | href=https://meteo.gov.ua/ua/33345/hydrology/hydr_water_level_changes_map/'
+  puts lines.join("\n")
+rescue
+  puts "Немає даних постів спостережень!"
+end
